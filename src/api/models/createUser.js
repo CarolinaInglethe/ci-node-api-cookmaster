7bae866184172272133ca020232c749ec7d6ddd6
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-17 17:40:57 -0300

    refatora CreateUser -Camada Models

--- src/api/models/createUser.js
+++ src/api/models/createUser.js
@@ -1,11 +1,13 @@
 const connection = require('./connection');
 
 module.exports = async (data) => {
+    const { name, email } = data;
+
     const Users = await connection()
       .then((db) => db.collection('users'));
   
     const { insertedId: _id } = await Users
-    .insertOne({ name: data.name, email: data.email, role: 'user' });
+    .insertOne({ name, email, role: 'user' });
   
-    return { user: { _id, ...data } };
+    return { user: { _id, name, email, role: 'user' } };
   };
