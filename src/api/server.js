0604f6aaf3d45a2968fb601afa682c336aa89b01
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-17 20:25:43 -0300

    Requesito 3 - Cria/cadastra Receita - Rota e arquivos das camadas

--- src/api/server.js
+++ src/api/server.js
@@ -5,9 +5,12 @@ require('dotenv').config();
 
 const controllersCreateUser = require('./controllers/createUser');
 const controllersLogin = require('./controllers/login');
+const controllersCreateRecipe = require('./controllers/createRecipe');
+const auth = require('./middlewares/auth');
 
 app.post('/users', controllersCreateUser);
 app.post('/login', controllersLogin);
+app.post('/recipes', auth, controllersCreateRecipe);
 
 // ---------
 
