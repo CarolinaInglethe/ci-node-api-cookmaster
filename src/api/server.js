45143606ea1f1e6d98af7215e3340fb4eeba1f09
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-17 15:52:39 -0300

    Req 1 - Cadastro de usuarios - Rota users

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,14 @@
 const app = require('./app');
 require('dotenv').config();
 
+// --------
+
+const controllersCreateUser = require('./controllers/createUser');
+
+app.post('/users', controllersCreateUser);
+
+// ---------
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
