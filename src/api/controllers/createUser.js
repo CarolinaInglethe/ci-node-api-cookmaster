1e8b5be34fe18b41971b31af7ebd550883bb46ad
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-17 16:06:52 -0300

    Req 1 cadastra usuario /users - Camada controllers

---
+++ src/api/controllers/createUser.js
@@ -0,0 +1,17 @@
+const servicesCreateUser = require('../services/createUser');
+
+module.exports = async (req, res) => {
+  try {
+    const data = req.body;
+
+    const createdUser = await servicesCreateUser(data);
+
+    if (!createdUser) {
+      return res.status(400).json(createdUser); 
+    }
+
+    return res.status(201).json(createdUser);
+  } catch (err) {
+    return res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+};
