095725dd51bae31d6e073b351c5c4d1f7fc728ff
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-18 19:28:09 -0300

    Requesito 8 - Delete Recipe - Rota e arquivos

--- src/api/server.js
+++ src/api/server.js
@@ -9,15 +9,16 @@ const controllersCreateRecipe = require('./controllers/createRecipe');
 const controllersGetAllRecipes = require('./controllers/getAllRecipes');
 const controllersGetRecipeById = require('./controllers/getRecipeById');
 const controllersUpdateRecipe = require('./controllers/updateRecipe');
+const controllersDeleteRecipe = require('./controllers/deleteRecipe');
 const auth = require('./middlewares/auth');
 
 // requesito 1:
 app.post('/users', controllersCreateUser);
 // requesito 2 :
 app.post('/login', controllersLogin);
+
 // requesito 3:
 app.post('/recipes', auth, controllersCreateRecipe);
-
 // requesito 4:
 app.get('/recipes', controllersGetAllRecipes);
 // requesito 5:
@@ -27,6 +28,8 @@ app.get('/recipes/:id', controllersGetRecipeById);
 
 // Requesito 7 :
 app.put('/recipes/:id', auth, controllersUpdateRecipe);
+// Requesito 8 :
+app.delete('/recipes/:id', auth, controllersDeleteRecipe);
 
 // ---------
 
