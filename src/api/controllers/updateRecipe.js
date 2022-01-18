d4493e41a266a5ecb29e6b2983ef88ca7a37e367
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-18 19:12:51 -0300

    req 7 - refatora services e controllers

--- src/api/controllers/updateRecipe.js
+++ src/api/controllers/updateRecipe.js
@@ -3,12 +3,13 @@ const servicesUpdateRecipe = require('../services/updateRecipe');
 module.exports = async (req, res) => {
   try {
     const { id } = req.params;
+    const { authorization } = req.headers;
     
-    const updateRecipe = await servicesUpdateRecipe(req.body, id);
+    const updateRecipe = await servicesUpdateRecipe(req.body, id, authorization);
 
-    if (updateRecipe.message) {
-      return res.status(404).json(updateRecipe);
-    }
+    // if (updateRecipe.message) {
+    //   return res.status(404).json(updateRecipe);
+    // }
 
     return res.status(200).json(updateRecipe);
   } catch (err) {
--- src/api/services/updateRecipe.js
+++ src/api/services/updateRecipe.js
@@ -1,7 +1,10 @@
 const modelsUpdateRecipe = require('../models/updateRecipe');
+const authService = require('./utils/authService');
 
-module.exports = async (data, id) => {
+module.exports = async (data, id, token) => {
+  const tokenInfoUser = await authService.verifyToken(token);
+
   const updateRecipe = await modelsUpdateRecipe(data, id);
 
-  return updateRecipe;
+  return { ...updateRecipe, userId: tokenInfoUser.id };
 };
