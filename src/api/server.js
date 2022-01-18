671d0f73305619e59d972c359163799d7f489def
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-18 16:02:23 -0300

    Requesito  4 - GetAllRecipes - Rota e arquivos

--- src/api/server.js
+++ src/api/server.js
@@ -6,11 +6,18 @@ require('dotenv').config();
 const controllersCreateUser = require('./controllers/createUser');
 const controllersLogin = require('./controllers/login');
 const controllersCreateRecipe = require('./controllers/createRecipe');
+const controllersGetAllRecipes = require('./controllers/getAllRecipes');
 const auth = require('./middlewares/auth');
 
+// requesito 1:
 app.post('/users', controllersCreateUser);
+// requesito 2 :
 app.post('/login', controllersLogin);
+// requesito 3:
 app.post('/recipes', auth, controllersCreateRecipe);
+
+// requesito 4:
+app.get('/recipes', controllersGetAllRecipes);
 
 // ---------
 
