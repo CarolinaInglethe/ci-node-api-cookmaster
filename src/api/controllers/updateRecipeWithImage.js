9c17445931feced3274a7dd1857350bbed47f2d4
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-19 18:08:53 -0300

    refatora req 9

--- src/api/controllers/updateRecipeWithImage.js
+++ src/api/controllers/updateRecipeWithImage.js
@@ -3,19 +3,16 @@ const servicesUpdateRecipeWithImage = require('../services/updateRecipeWithImage
 module.exports = async (req, res, _next) => {
     try {
       const { id } = req.params;
+      const { filename } = req.file;
       const { authorization } = req.headers;
-
-      const imagePath = `localhost:3000/src/uploads/${id}.jpeg`;
     
-      const updateRecipeWithImage = await servicesUpdateRecipeWithImage(
-        id, imagePath, authorization,
+      const addImageRecipe = await servicesUpdateRecipeWithImage(
+        id, filename, authorization,
       );
 
-      if (updateRecipeWithImage.message) {
-        return res.status(401).json(updateRecipeWithImage); 
-      }
+      if (addImageRecipe.message) return res.status(401).json(addImageRecipe);
 
-      return res.status(200).json(updateRecipeWithImage);
+      return res.status(200).json(addImageRecipe);
     } catch (err) {
       return res.status(500).json({ message: 'Erro interno do servidor' });
     }
--- src/api/services/updateRecipeWithImage.js
+++ src/api/services/updateRecipeWithImage.js
@@ -2,25 +2,20 @@ const modelsUpdateRecipeWithImage = require('../models/updateRecipeWithImage');
 const authService = require('./utils/authService');
 const modelsGetRecipeById = require('../models/getRecipeById');
 
-module.exports = async (id, imagePath, token) => {
+module.exports = async (id, filename, token) => {
   const tokenInfoUser = await authService.verifyToken(token);
 
-  if (!tokenInfoUser.id) return { message: 'missing auth token' };
+  const imagePath = `localhost:3000/src/uploads/${filename}`;
 
-  // busca receita pelo id e pega o userId dela:
-  const getRecipeById = await modelsGetRecipeById(id);
-  if (!getRecipeById) return { message: 'recipe not found' };
-  const { userId } = getRecipeById;
+  const { name, ingredients, preparation, userId } = await modelsGetRecipeById(id);
 
-  // (se ele nao for admin ou nao for author da receita ele 
-  // não pode editar) 
-  // verifica role admin , ou se id é mesmo que userId da receita:
-  if (tokenInfoUser.role !== 'admin' || tokenInfoUser.id !== userId) {
-    return { message: 'invalid user' };
+  if (userId !== tokenInfoUser.id && tokenInfoUser.role !== 'admin') {
+    return { message: 'missing auth token' };
   }
-  
-  // atualiza receita com a imagem apos verificaçoes com sucesso:
+
   await modelsUpdateRecipeWithImage(id, imagePath);
 
-  return { ...getRecipeById, image: imagePath };
+  const objRecipeWithImage = { _id: id, name, ingredients, preparation, userId, image: imagePath };
+
+  return objRecipeWithImage;
 };
