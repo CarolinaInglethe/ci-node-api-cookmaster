a4cd9b60202daa5e07cfdf659dd40c4fcd1b94f6
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-19 14:56:39 -0300

    req 9 - camada controllers

--- src/api/controllers/updateRecipeWithImage.js
+++ src/api/controllers/updateRecipeWithImage.js
@@ -1,3 +1,21 @@
-module.exports = async (_req, _res, _next) => {
-  
+const servicesUpdateRecipeWithImage = require('../services/updateRecipeWithImage');
+
+module.exports = async (req, res, _next) => {
+    try {
+      const { id } = req.params;
+      const { image } = req;
+      const { authorization } = req.headers;
+    
+      const updateRecipeWithImage = servicesUpdateRecipeWithImage(
+        id, image, authorization,
+      );
+    
+      // if (updateRecipeWithImage.message) {
+      //   return res.status(404).json(updateRecipeWithImage);
+      // }
+    
+      return res.status(200).json(updateRecipeWithImage);
+    } catch (err) {
+      return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
 };
